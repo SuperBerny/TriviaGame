49708eb7145127a10290251f2493e06c8ee246c2
Brian Patrick <batpat772@gmail.com>
2017-08-28 21:21:44 -0700

    more stuff

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+//I need to have a theme
+// I need to have a group of questions related to that theme
+// I need also need to have groups of answers related the questions
+      //I'll need a correct answer
+      //I'll also need incorrect answers
+//Every question will have 30 seconds to answer
+//Everytime a question is loaded the timer needs to reset to 30 seconds
+//The end of the game will display the results
+      //This will include
+         /*Wins
+           Losses
+           Unanswered questions
+         */
+
+
+         //The theme is going to be based around Subaru
+var questionsAndAnswers = {
+   "question1": "What was the first year the Subaru Impreza was sold in the United States?",
+   "answers1": ["1996","2004","1992","1989"],
+   "question2": "What does the accronym \"STi\" stand for?",
+   "answers2": ["Subaru Technica International","Subaru Technologies Institute","It isn\'t and accronym...","Subaru Technology Innovation"],
+   "question3": "Which cars are built on the Impreza platform?",
+   "answers3": ["Impreza,Forester,Outback","Outback,Forester,WRX","Legacy,Impreza,Tribeca","Impreza, Forester, Crosstek"],
+   "question4": "Most Subaru engines are 4 cylinders?",
+   "answers4": ["True","False"]
+}
