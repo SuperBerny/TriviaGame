49b336c4903fcbd13a06d997e038359add5d924c
Brian Patrick <batpat772@gmail.com>
2017-09-01 19:35:55 -0700

    haven't pushed anything in a while

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,16 +19,14 @@
 /******************/
 
 //this is the countdown variable
-var count = 30;
-
-//this is variable for stopping the countdown
-// var counter = window.setInterval(decrement, 1000);
+var count = 3;
+var timeInterval;
 
 //correct, incorrect, and unaswered tallies
 var correctTally = 0;
 var incorrectTally = 0;
 var unanswered = 0;
-var i = 5;
+var i = 0;
 
 //array of objects for questions and answers with incorrect answers in the objects
 
@@ -39,15 +37,17 @@ var question1 =
    b: "Kevin",
    c: "Kal-El",
    d: "Korben",
-   correct: "c"
+   correct: "c",
+   image: ""
 };
 var question2 =  {
-   question: "Who is the Green Lantern\' arch nemesis?",
+   question: "Who is the Green Lantern's arch nemesis?",
    a: "Doomsday",
    b: "Sinestro",
    c: "Blue Lantern",
    d: "Parallax",
-   correct: "b"
+   correct: "b",
+   image: ""
 };
 var question3 = {
    question: "Where does the Flash get his powers from?",
@@ -55,7 +55,8 @@ var question3 = {
    b: "Lightning",
    c: "He generates it himself, dummy",
    d: "The Speed Force",
-   correct: "d"
+   correct: "d",
+   image: ""
 };
 
 var question4 = {
@@ -64,7 +65,8 @@ var question4 = {
    b: "Siblings",
    c: "Cousins",
    d: "Biological Mother",
-   correct: "c"
+   correct: "c",
+   image: ""
 };
 var question5 = {
    question: "Who created Wonder Woman?",
@@ -72,7 +74,8 @@ var question5 = {
    b: "She was created the normal way?!?",
    c: "Hades",
    d: "Kronos",
-   correct: "a"
+   correct: "a",
+   image: ""
 };
 var question6 =  {
    question: "Who killed Superman?",
@@ -80,24 +83,16 @@ var question6 =  {
    b: "Lex Luthor",
    c: "Doomsday",
    d: "Dark Seid",
-   correct: "c"
+   correct: "c",
+   image: ""
 };
 
 var questions = [question1, question2, question3, question4, question5, question6];
-
+var picture = questions[i].image;
+var imagePlacer = $("<img>").attr("src", picture);
 /***********/
 /*functions*/
 /***********/
-
-//countdown function with 30 second countdown using var "count"
-// function decrement(){
-//    count--;
-//    $("#timer").html("<h2>" + count + "</h2>");
-//    if(count === 0){
-//          clearInterval(counter);
-//          alert("Times Up!");
-//    }
-// }
 
 function render(){
 //checking for game over case
@@ -106,46 +101,69 @@ if (i > questions.length - 1) {
 }
 
 $("#questions").html("<h2 class='question'>" + questions[i].question + "</h2>");
-   $("#a").text(questions[i].a);
-   $("#b").text(questions[i].b);
-   $("#c").text(questions[i].c);
-   $("#d").text(questions[i].d);
+$("#a").text(questions[i].a);
+$("#b").text(questions[i].b);
+$("#c").text(questions[i].c);
+$("#d").text(questions[i].d);
+$("#jumbotron").append(imagePlacer);
 }
 
+//calling render populates each button and the question div in the html
 render();
 
-var countDown = setInterval(function(){
-count--;
-$("#timer").html("<h2>" + count + "</h2>");
-if (count === 0){
-   clearInterval(countDown);
+function countDown(){
+    timeInterval = setInterval(function(){
+
+      if (count < 0){
+        unanswered++;
+        
+        i++;
+        count = 3;
+        countDown();
+        
+        render();
+        clearInterval(timeInterval);
+      }
+
+      $("#timer").html("<h2>" + count + "</h2>");
+      count--;
+   }, 1000);
 }
-}, 1000);
+
+countDown();
 
 $(".button").on("click", function(){
+clearInterval(timeInterval);
+count = 3;
+// countDown();
 var userAnswer = $(this).eq(0).attr("value");
 // console.log(userAnswer, questions[i].correct);
 var gameOver = i === questions.length - 1;
 
-if (gameOver) {
-   alert('game over');
+// Case user correct answer
+if (userAnswer === questions[i].correct){
+  correctTally++;
+// Case user incorrect answer
+} else {
+  incorrectTally++;
+}
+
+if (gameOver = i === questions.length -1) {
+   $(".button , #questions , #timer").remove();
+   $("#lastpage").append("<h2>" + "Correct Answers: " + correctTally + "</h2>")
+   .append("<h2>" + "Incorrect Answers: " + incorrectTally + "</h2>")
+   .append("<h2>" + "Unanswered Questions: " + unanswered + "</h2>");
    // show correct/incorrect answers tallies
 } else {
    i++;
    render();
 }
-// Case user correct answer
-if (userAnswer === questions[i].correct){
-   correctTally++;
-// Case user incorrect answer
-} else {
-   incorrectTally++;
-}
 
-// countDown = setInterval(decrement, 30000);
-console.log(correctTally, incorrectTally);
 
-});
+/**/
+}); //this is the on click end bracket
+/**/
 
 // show correct/incorrect on game over
-// 
+ 
+
